a048c4cb6785a0b0acced9feefe1f5c1611aa0b5
Philip Thomas <philip@yunait.com>
2013-05-09 14:03:03 +0200

    my own modifications: makes the code coherent with docs and HTML5 valid

--- app/js/directives.js
+++ app/js/directives.js
@@ -2,10 +2,17 @@
 
 /* Directives */
 
-
+/*
 angular.module('myApp.directives', []).
   directive('appVersion', ['version', function(version) {
     return function(scope, elm, attrs) {
       elm.text(version);
     };
   }]);
+*/
+
+myApp.directive('appVersion', function(version) {
+    return function(scope, elm, attrs) {
+      elm.text(version);
+    };
+});
--- app/js/filters.js
+++ app/js/filters.js
@@ -1,10 +1,16 @@
 'use strict';
 
 /* Filters */
-
+/*
 angular.module('myApp.filters', []).
   filter('interpolate', ['version', function(version) {
     return function(text) {
       return String(text).replace(/\%VERSION\%/mg, version);
     }
   }]);
+*/
+myApp.filter('interpolate', function(version) {
+    return function(text) {
+      return String(text).replace(/\%VERSION\%/mg, version);
+    }
+});
